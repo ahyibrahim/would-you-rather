840f7b7b11380e5381ca017f1925d7d7e3a88c02
ahyibrahim <57627482+ahyibrahim@users.noreply.github.com>
2021-07-16 20:30:03 +0200

    completed the redux store

---
+++ src/Store/index.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducers } from "redux";
+import { authedUserReducer } from "./Reducers/AuthedUserReducer";
+import { questionReducer } from "./Reducers/QuestionsReducer";
+import { userReducer } from "./Reducers/UsersReducer";
+import { middleware } from "./Middleware";
+
+const rootReducer = combineReducers({
+  userReducer,
+  questionReducer,
+  authedUserReducer,
+});
+
+export const store = createStore(rootReducer, middleware);
