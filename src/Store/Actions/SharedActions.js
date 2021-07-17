c595f8c6df74a04c47106bf6f49ecda27780dcc3
ahyibrahim <57627482+ahyibrahim@users.noreply.github.com>
2021-07-17 16:41:21 +0200

    Added Login Page

--- src/Store/Actions/SharedActions.js
+++ src/Store/Actions/SharedActions.js
@@ -3,7 +3,7 @@ import { receiveUsers } from "./UserActions";
 import { receiveQuestions } from "./QuestionActions";
 import { setAuthedUser } from "./AuthedUserActions";
 
-export function handelDataInit(dispatch) {
+export function handleDataInit(dispatch) {
   return getInitialData().then(({ users, questions }) => {
     dispatch(receiveUsers(users));
     dispatch(receiveQuestions(questions));
